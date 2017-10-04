c5ca9f87f6c8ed3950e333c4c6c2d0943c600961
Mateusz Żółtak <zozlak@zozlak.org>
2017-10-04 14:18:52 +0000

    Important fixes in the Proxy class: - the input stream is opened only if the request headers indicate existence of the request body (content-length and/or content-type headers exist) - all request headers are proxied by default with exceptions being explicitely listed   (previously request headers were skipped by default and only explicitely handled headers were proxied)

--- src/acdhOeaw/doorkeeper/Proxy.php
+++ src/acdhOeaw/doorkeeper/Proxy.php
@@ -21,6 +21,17 @@ use acdhOeaw\util\RepoConfig as RC;
  */
 class Proxy {
 
+    /**
+     * Response headers not to be forwarded to the client.
+     * @var array
+     */
+    static $skipResponseHeaders = array('transfer-encoding', 'host');
+
+    /**
+     * Gets an original (before being proxied) HTTP header value.
+     * @param string $header
+     * @return string
+     */
     static public function getHeader(string $header): string {
         $tmp = filter_input(\INPUT_SERVER, 'HTTP_X_FORWARDED_' . $header);
         if (empty($tmp)) {
@@ -32,12 +43,45 @@ class Proxy {
         return $tmp;
     }
 
+    /**
+     *
+     * @var \acdhOeaw\doorkeeper\Doorkeeper
+     */
     private $doorkeeper;
 
+    /**
+     * Headers not to be automatically included in the proxied request.
+     * 
+     * Typically they have a separate logic driving their values.
+     * @var arrray
+     * @see getForwardHeaders()
+     */
+    private $skipForwardHeaders = array(
+        'authorization',
+        'x-forwarded-for',
+        'x-forwarded-proto',
+        'x-forwarded-host',
+        'x-forwarded-port',
+        'x-forwarded-server',
+        'host'
+    );
+
+    /**
+     * 
+     * @param \acdhOeaw\doorkeeper\Doorkeeper $d
+     */
     public function __construct(Doorkeeper $d) {
-        $this->doorkeeper = $d;
+        $this->doorkeeper           = $d;
+        $this->skipForwardHeaders[] = strtolower(RC::get('fedoraRolesHeader'));
     }
 
+    /**
+     * 
+     * @param string $url
+     * @param \acdhOeaw\doorkeeper\ProxyOptions $opts
+     * @return Response
+     * @throws RequestException
+     */
     public function proxy(string $url, ProxyOptions $opts = null): Response {
         if ($opts === null) {
             $opts = new ProxyOptions();
@@ -49,59 +93,24 @@ class Proxy {
             return;
         }
 
-        $method = strtoupper(filter_input(INPUT_SERVER, 'REQUEST_METHOD'));
-        $input  = $method !== 'HEAD' ? fopen('php://input', 'r') : null;
+        $method        = strtoupper(filter_input(INPUT_SERVER, 'REQUEST_METHOD'));
+        $contentType   = filter_input(INPUT_SERVER, 'CONTENT_TYPE');
+        $contentLength = filter_input(INPUT_SERVER, 'CONTENT_LENGTH');
+        $input         = null;
+        if ($method !== 'TRACE' && ($contentType !== null || $contentLength !== null)) {
+            $input = fopen('php://input', 'r');
+        }
 
         $options               = array();
         $output                = fopen('php://output', 'w');
         $options['sink']       = $output;
         $options['on_headers'] = function(Response $r) {
-            $this->handleHeaders($r);
+            $this->handleResponseHeaders($r);
         };
         $options['verify']          = false;
         $options['allow_redirects'] = $opts->allowRedirects;
         $client                     = new Client($options);
-
-        $contentType = filter_input(INPUT_SERVER, 'HTTP_CONTENT_TYPE');
-        $contentType = $contentType ? $contentType : filter_input(INPUT_SERVER, 'CONTENT_TYPE');
-
-        $contentDisposition = filter_input(INPUT_SERVER, 'HTTP_CONTENT_DISPOSITION');
-        $contentDisposition = $contentDisposition ? $contentDisposition : filter_input(INPUT_SERVER, 'CONTENT_DISPOSITION');
-
-        $headers = array(
-            'Accept'              => filter_input(INPUT_SERVER, 'HTTP_ACCEPT'),
-            'Content-Type'        => $contentType,
-            'Content-Disposition' => $contentDisposition
-        );
-        if (filter_input(INPUT_SERVER, 'HTTP_SLUG')) {
-            $headers['Slug'] = filter_input(INPUT_SERVER, 'HTTP_SLUG');
-        }
-        if ($opts->authHeaders) {
-            $authData   = Auth::authenticate();
-            $cfgPrefix  = $authData->admin ? '' : 'Guest';
-            $authHeader = Auth::getHttpBasicHeader(RC::get('fedora' . $cfgPrefix . 'User'), RC::get('fedora' . $cfgPrefix . 'Pswd'));
-
-            $headers['Authorization']              = $authHeader;
-            $headers[RC::get('fedoraRolesHeader')] = implode(',', $authData->roles);
-        }
-        if ($opts->proxyHeaders) {
-            $headers['X-FORWARDED-FOR']    = self::getHeader('FOR');
-            $headers['X-FORWARDED-PROTO']  = self::getHeader('PROTO');
-            $headers['X-FORWARDED-HOST']   = self::getHeader('HOST');
-            $headers['X-FORWARDED-PORT']   = self::getHeader('PORT');
-            $headers['X-FORWARDED-SERVER'] = self::getHeader('SERVER');
-        }
-        if ($opts->preserveHost) {
-            $tmp             = explode(', ', self::getHeader('HOST'));
-            $headers['Host'] = trim($tmp[0]);
-        }
-        if ($opts->cookies) {
-            $headers['Cookie'] = array();
-            foreach ($_COOKIE as $k => $v) {
-                $headers['Cookie'][] = $k . '=' . $v;
-            }
-            $headers['Cookie'] = implode('; ', $headers['Cookie']);
-        }
+        $headers                    = $this->getForwardHeaders($opts);
 
         //$this->doorkeeper->log(json_encode($headers));
         $request = new Request($method, $url, $headers, $input);
@@ -128,9 +137,11 @@ class Proxy {
         return $response;
     }
 
-    public function handleHeaders(Response $response) {
-        //@TODO for sure this list should be longer!
-        static $skipHeaders = array('transfer-encoding', 'host');
+    /**
+     * 
+     * @param Response $response
+     */
+    public function handleResponseHeaders(Response $response) {
 
         $status = $response->getStatusCode();
         if (in_array($status, array(401, 403))) {
@@ -143,7 +154,7 @@ class Proxy {
         } else {
             header('HTTP/1.1 ' . $response->getStatusCode() . ' ' . $response->getReasonPhrase());
             foreach ($response->getHeaders() as $name => $values) {
-                if (in_array(strtolower($name), $skipHeaders)) {
+                if (in_array(strtolower($name), self::$skipResponseHeaders)) {
                     continue;
                 }
                 foreach ($values as $value) {
@@ -151,6 +162,55 @@ class Proxy {
                 }
             }
         }
+    }
+
+    /**
+     * 
+     * @param \acdhOeaw\doorkeeper\ProxyOptions $opts
+     * @return array
+     */
+    private function getForwardHeaders(ProxyOptions $opts): array {
+        $headers = array();
+        foreach ($_SERVER as $k => $v) {
+            if (substr($k, 0, 5) !== 'HTTP_') {
+                continue;
+            }
+            $k = str_replace('_', '-', strtolower(substr($k, 5)));
+            if (!in_array($k, $this->skipForwardHeaders)) {
+                $headers[$k] = $v;
+            }
+        }
+
+        if ($opts->authHeaders) {
+            $authData   = Auth::authenticate();
+            $cfgPrefix  = $authData->admin ? '' : 'Guest';
+            $authHeader = Auth::getHttpBasicHeader(RC::get('fedora' . $cfgPrefix . 'User'), RC::get('fedora' . $cfgPrefix . 'Pswd'));
+
+            $headers['Authorization']              = $authHeader;
+            $headers[RC::get('fedoraRolesHeader')] = implode(',', $authData->roles);
+        }
+        if ($opts->proxyHeaders) {
+            $headers['x-forwarded-for']    = self::getHeader('FOR');
+            $headers['x-forwarded-proto']  = self::getHeader('PROTO');
+            $headers['x-forwarded-host']   = self::getHeader('HOST');
+            $headers['x-forwarded-port']   = self::getHeader('PORT');
+            $headers['x-forwarded-server'] = self::getHeader('SERVER');
+        }
+        if ($opts->preserveHost) {
+            $tmp             = explode(', ', self::getHeader('HOST'));
+            $headers['host'] = trim($tmp[0]);
+        }
+        if ($opts->cookies) {
+            $cookies = array();
+            foreach ($_COOKIE as $k => $v) {
+                $cookies[] = $k . '=' . $v;
+            }
+            if (count($cookies) > 0) {
+                $headers['cookie'] = implode('; ', $cookies);
+            }
+        }
+
+        return $headers;
     }
 
 }
