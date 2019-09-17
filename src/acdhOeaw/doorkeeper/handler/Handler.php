55d4339fa8ac5fbcff2d2a42e2ddf8606a107ea5
Mateusz Żółtak <mateusz.zoltak@oeaw.ac.at>
2019-09-17 14:19:48 +0200

    Avoid PID generation when they exist made right

--- src/acdhOeaw/doorkeeper/handler/Handler.php
+++ src/acdhOeaw/doorkeeper/handler/Handler.php
@@ -447,9 +447,14 @@ class Handler {
     static private function maintainPid(FedoraResource $res, Doorkeeper $d): bool {
         $pidProp = RC::get('epicPidProp');
 
+        $pidExists = false;
+        foreach ($res->getIds() as $i) {
+            $pidExists |= strpos((string) $i, RC::get('epicResolver')) === 0;
+        }
+
         $ret      = false;
         $metadata = $res->getMetadata();
-        if ($metadata->getLiteral($pidProp) !== null && $metadata->getResource($pidProp) === null) {
+        if ($metadata->getLiteral($pidProp) !== null && !$pidExists) {
             if (RC::get('epicPswd') === '') {
                 $d->log('  skipping PID generation - no EPIC password provided');
                 return false;
